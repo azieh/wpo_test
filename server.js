e1e07302da3125765502b4fd910ba43dbe0650a1
kwasniew <kwasniewski.mateusz@gmail.com>
2018-10-06 13:31:25 +0200

    automation with npm and parcel

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var compression = require("compression");
 var app = express();
 
 app.use(compression());
-app.use(express.static(__dirname));
+app.use(express.static("dist"));
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
