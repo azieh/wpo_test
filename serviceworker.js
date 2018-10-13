f61d07d2469c0a942e77cdbb365739208271db93
kwasniew <kwasniewski.mateusz@gmail.com>
2018-10-13 16:44:26 +0200

    install and activate added

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,14 @@
 console.log("SW script run at " + new Date());
 
-self.addEventListener("fetch", function(event) {
-    console.log("Fetching", event.request);
+self.addEventListener("install", function(event) {
+    console.log("SW installed at", new Date().toLocaleTimeString());
 });
+
+self.addEventListener("activate", function(event) {
+    console.log("SW activated at", new Date().toLocaleTimeString());
+});
+
+self.addEventListener("fetch", function(event) {
+    // console.log("Fetching", event.request);
+});
+
