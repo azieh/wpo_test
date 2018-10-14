fa7033c8d2c815c2a66a7043defcb36fd0b85707
kwasniew <kwasniewski.mateusz@gmail.com>
2018-10-14 13:21:47 +0200

    skipWaiting

--- serviceworker.js
+++ serviceworker.js
@@ -4,8 +4,8 @@ console.log("SW version "+ version +" script run at", new Date());
 
 self.addEventListener("install", function(event) {
     console.log("SW version " + version + " installed at", new Date());
+    self.skipWaiting();
 });
-
 self.addEventListener("activate", function(event) {
     console.log("SW  version " + version + " activated at", new Date());
 });
